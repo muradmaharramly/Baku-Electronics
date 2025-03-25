daf913abe0970b118557be6f680c5960001989a2
muradmaharramly <mooradmaharramly@gmail.com>
2025-03-25 13:51:33 +0400

    news page slider edit

--- src/components/sliders/NewsPageSlider.jsx
+++ src/components/sliders/NewsPageSlider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 import { Link } from "react-router-dom";
@@ -27,46 +27,45 @@ function PrevArrow(props) {
   );
 }
 
-function NewsPageSlider({news}) {
-    const settings = {
-        dots: true,  // Dots-ları aktivləşdir
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 500,
-        autoplaySpeed: 3000,
-        cssEase: "linear",
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        customPaging: (i) => (
-          <div className="custom-dot"></div>  // Xüsusi dot görünüşü
-        ),
-        appendDots: dots => (
-          <div className="custom-dots-container">
-            <ul> {dots} </ul> 
-          </div>
-        )
-      };
-  
-    return (
-      <div className="slider-container">
-        <Slider {...settings}>
-        {news.map((item) => (
-            <div className="slide" key={item.id}>
-             <img src={item.image} alt={item.title}/>
-             <div className="info-div">
-                <span>{item.date.slice(0, 10)}</span>
-                <h1>{item.title.substring(0, 35)}...</h1>
-                <Link to={`/news/${slugify(item.title, { lower: true })}`}>Ətraflı bax</Link>
-             </div>
+function NewsPageSlider({ news }) {
+  const settings = {
+    dots: true,  // Dots-ları aktivləşdir
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    customPaging: (i) => (
+      <div className="custom-dot"></div>  // Xüsusi dot görünüşü
+    ),
+    appendDots: dots => (
+      <div className="custom-dots-container">
+        <ul> {dots} </ul>
+      </div>
+    )
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {news.slice(0, 8).map((item) => (
+          <div className="slide" key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <div className="info-div">
+              <span>{item.date.slice(0, 10)}</span>
+              <h1>{item.title.substring(0, 35)}...</h1>
+              <Link to={`/news/${slugify(item.title, { lower: true })}`}>Ətraflı bax</Link>
+            </div>
           </div>
         ))}
-          
-          
-        </Slider>
-      </div>
-    );
-  }
-  
-  export default NewsPageSlider;
+      </Slider>
+
+    </div>
+  );
+}
+
+export default NewsPageSlider;
